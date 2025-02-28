05413efe2d83aa97d424b18933b00365350bc283
LauritzCode <lauritzmazin@gmail.com>
2025-02-28 13:50:26 +0100

    Add computer player random move generation with tests

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -7,3 +7,14 @@ describe("Player", () => {
     expect(player.gameboard).toBeInstanceOf(GameBoard);
   });
 });
+
+describe("Computer Player", () => {
+  test("Should generate a legal move within board bounds (0 to 9", () => {
+    const computer = new Player("computer");
+    const move = computer.generateRandomMove();
+    expect(move[0]).toBeGreaterThanOrEqual(0);
+    expect(move[0]).toBeLessThan(10);
+    expect(move[1]).toBeGreaterThanOrEqual(0);
+    expect(move[1]).toBeLessThan(10);
+  });
+});
--- src/player.js
+++ src/player.js
@@ -1,8 +1,31 @@
 const GameBoard = require("./gameboard");
 
 class Player {
-  constructor() {
+  constructor(type = "human") {
+    this.type = type;
     this.gameboard = new GameBoard();
+
+    if (this.type === "computer") {
+      this.previousMoves = [];
+    }
+  }
+
+  generateRandomMove() {
+    let move = [];
+    // generate moves until -->
+
+    do {
+      let x = Math.floor(Math.random() * 10);
+      let y = Math.floor(Math.random() * 10);
+      move = [x, y];
+    } while (
+      this.previousMoves &&
+      this.previousMoves.some((m) => m[0] === move[0] && m[1] === move[1])
+    );
+    if (this.type === "computer") {
+      this.previousMoves.push(move);
+    }
+    return move;
   }
 }
 
